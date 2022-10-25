e7ac39d3712538a8abeffd19e6f977161c583ea6
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-10-25 23:10:14 +0600

    courses json file done

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
-const app = (express();
+const app = express();
 const port = process.env.PORT || 5000;
+
+const courses = require('./data/courses.json')
 
 
 app.get('/', (req, res) => {
     res.send("E-learning server is running");
 })
 
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+
+
 app.listen(port, () => {
-    console.log('Dragon News Server running on port', port);
+    console.log('E-learning Server running on port', port);
 })
 
 
