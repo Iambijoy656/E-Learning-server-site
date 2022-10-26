26f4f9c276825a9e233743f07436b5bd62d85952
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-10-26 12:16:59 +0600

    api done

--- index.js
+++ index.js
@@ -25,6 +25,23 @@ app.get('/courses', (req, res) => {
 })
 
 
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course_category = allCourse.filter(c => c.course_id === id)
+    res.send(course_category)
+})
+
+
+
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+
+    const selectedCourse = allCourse.find(c => c.id === id)
+    res.send(selectedCourse)
+
+})
+
 
 app.listen(port, () => {
     console.log('E-learning Server running on port', port);
